69b121f30f3d4c7bb4a9aff576a6bc0e0c0a4984
Pitt <karimovpg@gmail.com>
2019-03-13 16:30:33 +0200

    added modal window with inputs and animations

--- weight-app/src/App/App.jsx
+++ weight-app/src/App/App.jsx
@@ -1,25 +1,69 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import StartPage from '../StartPage/StartPage';
+import StartInputPage from '../StartInputPage/StartInputPage';
 
 class App extends Component {
 
   state = {
-    start: false
+    start: false,
+    modal: true,
+    dataArr: [],
+    currentWeight: '',
+    desirableWeight: '',
+    currentHeight: ''
   }
-
+  // start page ==========================
   startWeighing = () => {
     this.setState({
       start: true
     })
   }
+  // modal page ============================
+
+  inputChange = (e) => {
+
+    const name = e.target.name;
+    const value = e.target.value;
+
+    this.setState ({
+      [name]: value
+    })
+  }
+
+  collectData = (e) => {
+    
+    e.preventDefault();
+
+    const newObj = {
+      currentWeight: this.state.currentWeight,
+      desirableWeight: this.state.desirableWeight,
+      currentHeight: this.state.currentHeight,
+      id: Date.now()
+    }
+
+    this.setState (prev=>({
+      dataArr: [...prev.dataArr, newObj ],
+      currentWeight: '',
+      desirableWeight: '',
+      currentHeight: ''
+    }))
+  }
+
+  toggleModal = () => {
+    this.setState({
+      modal: !this.state.modal
+    })
+  }
 
   render() {
-    const {start} = this.state;
+    const {start, modal, currentWeight, desirableWeight, currentHeight} = this.state;
     return (
-      <Fragment>
+      <div className='App'>
         <StartPage startWeighing={this.startWeighing} start={start}></StartPage>
-      </Fragment>
+        <StartInputPage modal={modal} toggleModal={this.toggleModal} currentWeight={currentWeight} desirableWeight={desirableWeight} currentHeight={currentHeight} collectData={this.collectData} inputChange={this.inputChange}/>
+        <h1>Zalupa</h1>
+      </div>
     );
   }
 }
---
+++ weight-app/src/StartInputPage/StartInputPage.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './StartInputPage.css';
+
+const StartInputPage = ({modal, currentWeight, desirableWeight, currentHeight, collectData, inputChange, toggleModal}) => {
+    return (
+        <div className={modal?'StartInputPage':'StartInputPage StartInputPage--moveLeft'}>
+            <p className='StartInputPage__Text'><span className='StartInputPageButton--name'>Save weight</span></p>
+            <div className="StartInputPage__Container">
+                <form onSubmit={collectData} action="" className="StartInputPageContainer__Form">
+                    <label htmlFor="userWeight"><span>Your current weight(kg):</span>
+                    <input onChange={inputChange} type="text" name='currentWeight' value={currentWeight} className="StartInputPageContainer__Input"/>
+                    </label>
+                    <label htmlFor="userWantWeight"><span>Your desirable weight(kg):</span>
+                    <input onChange={inputChange} type="text" name='desirableWeight' value={desirableWeight} className="StartInputPageContainer__Input"/>
+                    </label>
+                    <label htmlFor="userHeight"><span>Your height(cm):</span>
+                    <input onChange={inputChange} type="text" name='currentHeight' value={currentHeight} className="StartInputPageContainer__Input"/>
+                    </label>
+                    <button onClick={toggleModal} type='submit' className="StartInputPageContainer__Button">Save</button>
+                </form>
+                    <button disabled type='button'className="StartInputPageContainer__Button">Cancel</button>
+            </div>
+        </div>
+    );
+};
+
+export default StartInputPage;
